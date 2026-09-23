81fd067a88a74b555f586e504c61a3e47e98080c
obbetz123-tech <obbetz123@gmail.com>
2026-09-23 12:53:08 -0700

    Update companies-data.ts

--- src/companies-data.ts
+++ src/companies-data.ts
@@ -66,7 +66,7 @@ export const companies: Company[] = [
     slug: 'olli',
     name: 'Olli Technologies',
     logo: 'olli-logo.svg', // sourced from their original olli-logo.svg (witholli.com/olli-logo.svg), re-rendered at high resolution and exported as a PNG
-    founder: 'Matt Simpson',
+    founder: 'Ben Book',
     location: 'Santa Barbara, CA',
     website: 'https://www.witholli.com',
     description:
